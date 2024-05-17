c2fdf1819a33f09a11366f3e743fc972c4eb2db0
Asus <ppaogolimlim@gmail.com>
2024-05-17 09:22:34 +0800

    LATESTTETETEST

---
+++ app/src/main/java/com/example/petbook/RecyclerViewInterface.java
@@ -0,0 +1,5 @@
+package com.example.petbook;
+
+public interface RecyclerViewInterface {
+    void onItemClick(int position);
+}
---
+++ app/src/main/java/com/example/petbook/RecyclerView_MainLayout.java
@@ -0,0 +1,14 @@
+package com.example.petbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecyclerView_MainLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view_main_layout);
+    }
+}
